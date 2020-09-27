9b81fe5b383d07fe360682d6a1ff9f1b9b47dcd6
Joshua Hoffman <joshuahoffman2023@u.northwestern.edu>
2020-09-26 23:10:59 -0500

    Initial Commit

---
+++ Shared/Type_CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  Type_CalcApp.swift
+//  Shared
+//
+//  Created by Joshua Hoffman on 9/26/20.
+//
+
+import SwiftUI
+
+@main
+struct Type_CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
